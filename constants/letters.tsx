f7daaa277ca3dc8139d79cac3068c1ba0c052903
Alexander <kontakt@a-wels.de>
2023-07-05 10:01:01 +0200

    More questions

--- constants/letters.tsx
+++ constants/letters.tsx
@@ -1,7 +1,7 @@
 // define all letters that can be chosen
 const letters = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I',
-    'J','K','L','M','N', 'O', 'P', 'Q', 'R', 'S', 
-    'T', 'U / V / W', 'X / Y / Z', 'SCH'];
+    'J','K','L','M','N', 'O', 'P', 'Q / I', 'R', 'S', 
+    'T', 'U', 'V', 'W', 'X / Y / Z', 'SCH'];
 
 export default letters;
